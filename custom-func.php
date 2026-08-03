c82a8f36b8b77e4071fa30ade6db3f8d064aacdf
Thiago de Mello Bueno <thiago@madeofpeople.org>
2026-08-03 18:20:08 +0000

    release(plugin): v0.19.0

--- custom-func.php
+++ custom-func.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Custom Func
  * Description:       Headless site functionality plugin for abcnorio.org.
- * Version:           0.18.0
+ * Version:           0.19.0
  * Requires PHP:      8.3
  * Author:            abcnorio
  * Text Domain:       custom-func
